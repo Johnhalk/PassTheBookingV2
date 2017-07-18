5d11eb7fad7bc2f23af06a688d8dbff689dc6a3e
John Ashton <John_ashton@hotmail.com>
2017-07-18 18:23:27 +0100

    Add edit and new functionality

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.client_list, name='client_list'),
     url(r'^client/(?P<pk>\d+)/$', views.client_detail, name='client_detail'),
+    url(r'^client/new/$', views.client_new, name='client_new'),
+    url(r'^client/(?P<pk>\d+)/edit/$', views.client_edit, name='client_edit')
 ]
--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .models import Client
+from .forms import ClientForm
 
 
 def client_list(request):
@@ -11,3 +12,37 @@ def client_list(request):
 def client_detail(request, pk):
     clients = get_object_or_404(Client, pk=pk)
     return render(request, 'booking/client_detail.html', {'clients': clients})
+
+def client_new(request):
+    if request.method == "POST":
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.clients = request.user
+            client.email = request.user
+            client.first_name = request.user
+            client.last_name = request.user
+            client.date_joined = timezone.now()
+            client.save()
+            return redirect('client_detail', pk=client.pk)
+    else:
+        form = ClientForm()
+        return render(request, 'booking/client_edit.html', {'form': form})
+
+
+def client_edit(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.clients = request.user
+            client.email = request.user
+            client.first_name = request.user
+            client.last_name = request.user
+            client.date_joined = timezone.now()
+            client.save()
+            return redirect('client_detail', pk=client.pk)
+    else:
+        form = ClientForm()
+        return render(request, 'booking/client_edit.html', {'form': form})
