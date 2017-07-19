240f40bf2af522ae5369a381f7c1ef6c88de8745
John Ashton <John_ashton@hotmail.com>
2017-07-19 13:47:18 +0100

    Add property detail view and model to property

--- property/urls.py
+++ property/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.property_list, name='property_list'),
+    url(r'^(?P<pk>\d+)/$', views.property_detail, name='property_detail'),
 ]
--- property/views.py
+++ property/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Property
 
 def property_list(request):
     propertys = Property.objects.all()
     return render(request, 'property/property_list.html', {'propertys': propertys})
+
+def property_detail(request, pk):
+    propertys = get_object_or_404(Property, pk=pk)
+    return render(request, 'property/property_detail.html', {'propertys': propertys})
