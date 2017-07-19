cc387cf41555b87f48dc11be9a12324e916fc54f
John Ashton <John_ashton@hotmail.com>
2017-07-19 13:24:35 +0100

    Add templates for property list

--- property/views.py
+++ property/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Property
 
-# Create your views here.
+def property_list(request):
+    propertys = Property.objects.all()
+    return render(request, 'property/property_list.html', {'propertys': propertys})
