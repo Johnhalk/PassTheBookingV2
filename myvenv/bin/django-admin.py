555819fb1adc91ae7afca97864f79f50e1943478
John Ashton <John_ashton@hotmail.com>
2017-07-22 21:28:57 +0100

    Removing all files from git ignore

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/johnashton/Desktop/Projects/Python/PassTheBooking/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
