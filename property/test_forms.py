d8340188de3c9b05d99ab02d0bd93b4678f43905
John Ashton <John_ashton@hotmail.com>
2017-07-23 09:45:13 +0100

    Add form tests for each model

---
+++ property/test_forms.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from .models import Property
+from .forms import PropertyForm
+
+class PropertyFormTestCase(TestCase):
+    fixtures=['initial_data.json']
+
+    def test_valid_data(self):
+        form=PropertyForm({
+            'owner': 2,
+            'location_city': 'Bristol',
+            'Address': '123 Test Street',
+            'number_of_bedrooms': '4',
+        })
+        self.assertTrue(form.is_valid())
+        propertys=form.save()
+        self.assertEqual(propertys.owner.pk, 2)
+        self.assertEqual(propertys.location_city, 'Bristol')
+        self.assertEqual(propertys.Address, '123 Test Street')
+        self.assertEqual(propertys.number_of_bedrooms, 4)
